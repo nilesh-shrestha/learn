abea5974ae9b417829a87730a399ab1a20be599a
nilesh-shrestha <nileshshrestha001@gmail.com>
2022-05-03 17:18:53 +0545

    simple change

--- esh.py
+++ esh.py
@@ -3,6 +3,7 @@ b = int(input())
 
 if (a<b):
     print("{} is greater than {}".format(a,b))
+elif (a==b):
+    print("{} is equal to {}".format(b,a))
 else:
     print("{} is greater than {}".format(b,a))
-
